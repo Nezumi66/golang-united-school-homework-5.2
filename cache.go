4bae940efb0935339a6bafdc0970a7bf3abd6ec8
Nezumi66 <nodirmirzo.tashniyazov@billz.uz>
2022-07-14 00:26:11 +0400

    Implemented Get method

--- cache.go
+++ cache.go
@@ -3,17 +3,33 @@ package cache
 import "time"
 
 type Cache struct {
+	cache map[string]Key
+}
+
+type Key struct {
+	value    string
+	deadline *time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	result := make(map[string]Key)
+	return Cache{cache: result}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (receiver Cache) Get(key string) (string, bool) {
+	result, ok := receiver.cache[key]
+	if !ok {
+		return "", false
+	}
+	if result.deadline.Before(time.Now()) {
+		delete(receiver.cache, key)
+		return "", false
+	}
+	return result.value, true
 }
 
 func (receiver) Put(key, value string) {
+
 }
 
 func (receiver) Keys() []string {
